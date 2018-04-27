766ef05b564fe77121fdbc65bcf05528485ac5c9
Anderson Laurentino <alaurentino.br@gmail.com>
2018-04-27 18:41:03 -0300

    Add Header and README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# ETE Cícero Dias - NAVE Recife
+# Steaph Copyright
+
+## Authors:
+* Anderson Laurentino
+
+That code is resposable to control the best robot
+of the OBR and Robocup Jr Rescue Line
--- nave-line-follow.ino
+++ nave-line-follow.ino
@@ -1,3 +1,12 @@
+/*
+ * ETE Cícero Dias - NAVE Recife
+ * Steaph Copyright
+ * Authors: Anderson Laurentino
+ * 
+ * That code is resposable to control the best robot
+ * of the OBR and Robocup Jr Rescue Line
+ */
+
 #include "MotorShield.h"
 #include "Sensors.h"
 
